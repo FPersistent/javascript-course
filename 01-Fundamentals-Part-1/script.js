ce0b91f5c407e29b0503e1ceb6b3de4f263146b1
Faysal Garaad <faysal.garaad01@gmail.com>
2021-03-24 23:24:58 -0400

    Update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,4 @@
-//Started at March/22/21 Day:Monday, Time: 4:10pm, Came back from work to get on with our new serious daily routine..
+//Started at March/25/21 Day:Thursday, Time: 5:10pm, Came back from work to get on with our new serious daily routine....
 let year;
 year = 1081;
 console.log(typeof year);
