1a73f7fd9bd7ba9bcbda83f21d834376d66c6e0f
Faysal Garaad <faysal.garaad01@gmail.com>
2021-03-25 23:07:35 -0400

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,3 @@
-//Started at March/25/21 Day:Thursday, Time: 5:10pm, Came back from work to get on with our new serious daily routine....
-let year;
-year = 1081;
-console.log(typeof year);
+//Started at March/25/21 Day:Thursday, Time: 11:00pm, Came back from work to get on with our new serious daily routine....
+
+// Values and Variables.
